cffaa69d5ce8afbc0614459cb023c839a27459e3
qianhongqiang <ericqian2009@gmail.com>
2026-03-18 21:17:37 +0800

    Initial Commit

---
+++ IR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IR
+//
+//  Created by hongqiang qian on 2026/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
